54d9141091b1698de2df73f54918dd481bc0ab50
Lea Mueller <mueller.lea@uni-jena.de>
2018-01-09 10:59:34 +0100

    changed diffbild.py

--- code/diffbild.py
+++ code/diffbild.py
@@ -2,6 +2,23 @@ import numpy as np
 import cv2
 import os
 import glob
+
+# Create difference images from video frames
+# Will be used for ball detection
+# path: folder containing frames
+
+path =  '/home/mueller/code/python/Anwendungspraktikum/Videos/GoPro/GoProFrames'
+imgtype = 'png'
+imgname = 'image_GP_'
+digits = '05'
+outdir = '/home/mueller/code/python/Anwendungspraktikum/Videos/GoPro/GoProFramesDiff/'
+
+files = sorted(glob.glob(path + '/*.' + imgtype))
+for i in range(1, len(files)):
+	diff = cv2.absdiff(cv2.imread(files[i]), cv2.imread(files[i-1]))
+	imgname = imgname + format(i, digits) + imgtype
+	cv2.imwrite(outdir + imgname, diff)
+
 '''
 path = os.getcwd() + '/../tennis_top_cat_mouse.mp4'
 print(path)
@@ -26,29 +43,3 @@ while(1):
 cap.release()
 cv2.destroyAllWindows()
 ''' 
-path =  '/home/mueller/code/python/Anwendungspraktikum/Videos/GoPro/GoProFrames'
-files = sorted(glob.glob(path + '/*.png'))
-print(files)
-#fgbg = cv2.createBackgroundSubtractorMOG2()
-#path = os.getcwd() + '/../tennis_top_cat_mouse.mp4'
-#cap = cv2.VideoCapture(path)
-for i in range(1, len(files)):
-#while(1):
-	#ret, frame = cap.read()
-	#ret_, frame_ = cap.read()
-	#diff = cv2.acsdiff(frame_, frame)
-	diff = cv2.absdiff(cv2.imread(files[i]), cv2.imread(files[i-1]))
-	imgname = 'image_GP_' + format(i, '05') + '.png'
-	print(imgname)
-	cv2.imwrite('/home/mueller/code/python/Anwendungspraktikum/Videos/GoPro/GoProFramesDiff/' + imgname, diff)
-	#image = cv2.imread(files[i])
-	#fgmask = fgbg.apply(image)
-	#cv2.imshow('diff', diff)
-	#plt.show()
-
-	#k = cv2.waitKey(20) & 0xff
-	#if k == 'q':
-	#	break
-
-#cv2.destroyAllWindows()
-
