cbd846e6ea96825d71793f52178877142240845d
Lea Mueller <mueller@argus4.inf-cv.uni-jena.de>
2018-03-15 20:46:18 +0100

    Performance measure for ball detection added

--- tensorflow/MakePrediction.py
+++ tensorflow/MakePrediction.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 import zipfile
 import cv2
 import glob
+import argparse
 
 from collections import defaultdict
 from io import StringIO
@@ -26,22 +27,49 @@ from utils import visualization_utils as vis_util
 # This is needed to display the images.
 #%matplotlib inline
 
-#'/home/mueller/code/python/Anwendungspraktikum/Videos/GoPro/GoProFrames'
-# Enter path to folder with frames of Video
-TEST_IMAGE_FILE = os.path.join(os.getcwd(), '../annotations/GoProBall_test.txt')
-PATH_TO_TEST_IMAGES_DIR = sys.argv[1]
-
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description = 'Ball Detection Prediction')
+	parser.add_argument('--PATH_TO_TEST_IMAGES_DIR', metavar='path to folder with images',
+	                    help='folder containing images on which prediction will be made')
+	parser.add_argument('--TEST_IMAGE_FILE', metavar = 'txt file with ground truth relativ from current directory',
+						help='if set all images in file will be used for detection task')
+	parser.add_argument('--Player', metavar = 'detect player in image', nargs=1,
+	                    help='if set model for player detection is used. Otherwise ball will be detected.')
+	args = parser.parse_args()
+	print(args)
 # What model to download.
 MODEL_DIR = '/home/mueller/code/python/Anwendungspraktikum/cvtennis/tensorflow/models/'
-MODEL_NAME = 'GoProSpielerGraph'
-LABEL_MAP = 'spieler_label_map.pbtxt'
-NUM_CLASSES = 2
-
-#For Ball
-if sys.argv[2] == 'Ball':
+if args.Player:
+	print('Player detection')
+	MODEL_NAME = 'GoProSpielerGraph'
+	LABEL_MAP = 'spieler_label_map.pbtxt'
+	NUM_CLASSES = 2
+	writefile = open('results/PositionsPlayer_.csv', 'a')
+else:
+	print('Ball detection')
 	MODEL_NAME = 'GoProBall1742Graph'
 	LABEL_MAP = 'ball_label_map.pbtxt'
 	NUM_CLASSES = 1
+	writefile = open('results/PositionsBall_.csv', 'a')
+
+# Enter path to folder with frames of Video
+print(args.TEST_IMAGE_FILE, args.PATH_TO_TEST_IMAGES_DIR)
+if args.TEST_IMAGE_FILE:
+	file = args.TEST_IMAGE_FILE
+	print(file)
+	TEST_IMAGE_FILE = os.path.join(os.getcwd(), file)
+	TEST_IMAGE_PATHS = []
+	with open(TEST_IMAGE_FILE) as f:
+		for row in f:
+			imgname = row.split(' ')[0]
+			TEST_IMAGE_PATHS += [os.path.join(os.path.join(os.getcwd(), 
+				'../../Videos/GoPro/GoProFrames'), imgname)]
+elif args.PATH_TO_TEST_IMAGES_DIR:
+	PATH_TO_TEST_IMAGES_DIR = args.PATH_TO_TEST_IMAGES_DIR
+	TEST_IMAGE_PATHS = sorted(glob.glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, '*.png')))
+else:
+	sys.exit('Missing Input! Either Testfile or folder with images necessary (use python MakePrediction.py -h)')
+
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_DIR + MODEL_NAME + '/frozen_inference_graph.pb'
@@ -59,27 +87,16 @@ with detection_graph.as_default():
 		tf.import_graph_def(od_graph_def, name='')
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, 
+	use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 def load_image_into_numpy_array(image):
 	(im_width, im_height) = image.size
 	return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
-# Get Video
-#TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image_GP_{:05}.png'.format(i)) for i in range(0, 3400) ]
-#TEST_IMAGE_PATHS = sorted(glob.glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, '*.png')))
-TEST_IMAGE_PATHS = []
-with open(TEST_IMAGE_FILE) as f:
-	for row in f:
-		imgname = row.split(' ')[0]
-		TEST_IMAGE_PATHS += [os.path.join(os.path.join(os.getcwd(), '../../Videos/GoPro/GoProFrames'), imgname)]
-#sorted(glob.glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, '*.png')))
-
 # Size, in inches, of the output images.
 IMAGE_SIZE = (8, 6)
-
-writefile = open('/home/mueller/code/python/Anwendungspraktikum/cvtennis/TestsetPositionsBall.csv', 'a')
 
 #cv2.startWindowThread()
 #cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
@@ -97,16 +114,11 @@ with detection_graph.as_default():
 		num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 		#plt.figure(figsize=IMAGE_SIZE)
 		for i in range(0,len(TEST_IMAGE_PATHS),1):
-			image_path1 = TEST_IMAGE_PATHS[i] #1-2
-			print(i, image_path1)
-			#image_path2 = TEST_IMAGE_PATHS[i-1]
-			image1 = cv2.imread(image_path1)
-			#image2 = cv2.imread(image_path2)
-			image = Image.open(image_path1)
+			image_path = TEST_IMAGE_PATHS[i] #1-2
+			print(i, image_path)
+			image = Image.open(image_path)
 			# the array based representation of the image will be used later in order to prepare the
 			# result image with boxes and labels on it.
-			#image_np1 = load_image_into_numpy_array(image1)
-			#image_np2 = load_image_into_numpy_array(image2)
 			image_np = load_image_into_numpy_array(image)
 			# Expand dimensions since the model expects images to have shape: [1, None, None, 3]
 			image_np_expanded = np.expand_dims(image_np, axis=0)
@@ -129,8 +141,13 @@ with detection_graph.as_default():
 			#plt.pause(.001)
 			#plt.draw()
 
-			writestr = '{},{},{},{},{}\n'.format(image_path1, boxes[0][0], scores[0][0], classes[0][0], num)
-			writefile.write(writestr)
+			if args.PATH_TO_TEST_IMAGES_DIR:
+				writestr = '{};[{} {} {} {} {}];{};{};{}\n'.format(image_path, boxes[0][0],boxes[0][1], boxes[0][2], 
+					boxes[0][3], boxes[0][4], scores[0][0:5], classes[0][0:5], num)
+				writefile.write(writestr)
+			elif args.TEST_IMAGE_FILE:
+				writestr = '{},{},{},{},{}\n'.format(image_path, boxes[0][0], scores[0][0], classes[0][0], num)
+				writefile.write(writestr)				
 			#if cv2.waitKey(24) & 0xFF==ord('q'):
 		#		break
 
---
+++ tensorflow/PerformanceOnTestSet.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+predictionsfile = '../TestsetPositionsBall.csv'
+testfile = '../annotations/GoProBall_train.txt'
+
+positions = []
+
+with open(predictionsfile, 'r') as f:
+
+	positions
+
